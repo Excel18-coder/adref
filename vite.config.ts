8b6c5c2a7ac9737c3b5e4b1ceb76f4753f048755
Excel18-coder <excel2130@gmail.com>
2026-04-17 12:40:19 -0400

    root directory configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,17 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [tsconfigPaths(), tanstackStart(), react(), tailwindcss()],
+	plugins: [
+		tsconfigPaths(),
+		tanstackStart({
+			prerender: {
+				enabled: true,
+				crawlLinks: true,
+				concurrency: 8,
+				failOnError: true,
+			},
+		}),
+		react(),
+		tailwindcss(),
+	],
 });
